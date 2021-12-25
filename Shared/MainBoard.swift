eba12d4150eea93b2df406df493ce9e3b724fce3
Jincheng <38003358+wroohtteeen123@users.noreply.github.com>
2021-12-26 04:34:46 +0800

    rwd

--- Shared/MainBoard.swift
+++ Shared/MainBoard.swift
@@ -39,7 +39,7 @@ struct MainBoard: View {
         
         let fmtr = DateFormatter()
         
-        fmtr.dateFormat = "yyyy-MM-dd ZZZZZ HH:mm:ss:SS"
+        fmtr.dateFormat = "yyyy年MM月dd日 HH时mm分ss秒"
         
         return fmtr
         
@@ -57,7 +57,7 @@ struct MainBoard: View {
                 .font(.largeTitle)
                 .fontWeight(.semibold)
             
-            Text(timeNow).font(.largeTitle).fontWeight(.black).multilineTextAlignment(.leading).lineLimit(/*@START_MENU_TOKEN@*/3/*@END_MENU_TOKEN@*/).frame(width: /*@START_MENU_TOKEN@*/700.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/).onReceive(timer) {
+            Text(timeNow).font(.largeTitle).fontWeight(.black).multilineTextAlignment(.leading).lineLimit(/*@START_MENU_TOKEN@*/3/*@END_MENU_TOKEN@*/).frame(width: 7000.0, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/).onReceive(timer) {
                 _ in self.timeNow = dateFormatter.string(from: Date())
             }
         }
